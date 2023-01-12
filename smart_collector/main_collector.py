cd4f8317e5f8698e6bc259c620c45e230a2fc54b
hanlimy <hanlimy@naver.com>
2023-01-12 23:15:58 +0900

    init : add some functions in main_collector.py

--- smart_collector/main_collector.py
+++ smart_collector/main_collector.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import pandas_datareader as pdr
 import matplotlib.pyplot as plt
 
 
@@ -9,11 +7,40 @@ def get_nasdaq_quandl():
     import quandl
     quandl.ApiConfig.api_key = "h4m1wXxBGk62tH6XfeWa"
 
-    df_gold = quandl.get("LBMA/GOLD", trim_start="2021-01-01", trim_end="2022-01-11")
+    date_start = '2021-01-01'
+    date_end = '2022-01-13'
+
+    df_gold = quandl.get("LBMA/GOLD", trim_start=date_start, trim_end=date_end)
+    df_silver = quandl.get("LBMA/SILVER", trim_start=date_start, trim_end=date_end)
+    df_copper = quandl.get("CHRIS/CME_HG10", trim_start=date_start, trim_end=date_end)
+    df_oil = quandl.get("OPEC/ORB", trim_start=date_start, trim_end=date_end)
+
     df_gold.to_csv('{}/df_gold.csv'.format(path_output))
 
     plt.rcParams["figure.figsize"] = (14, 4)
     plt.rcParams['axes.grid'] = True
 
 
-get_nasdaq_quandl()
+def get_kosdaq_fdr():
+    import FinanceDataReader as fdr
+
+    df_krx = fdr.StockListing('KRX')
+    df_appl = fdr.DataReader('AAPL', '2020-01-01', '2020-01-30')
+
+
+def get_kor_pdr():
+    from PublicDataReader import Kbland
+
+    api = Kbland()
+    params = {
+        "월간주간구분코드": "01",
+        "매물종별구분": "01",
+        "매매전세코드": "01",
+        # "지역코드": "11",
+        "기간": 1,
+    }
+    df = api.get_price_index(**params)
+    print(df.tail())
+
+
+get_kor_pdr()
