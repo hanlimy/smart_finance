1575fa2574fd5fb037582770907cfe790c79f80e
hanlimy <hanlimy@naver.com>
2023-01-13 16:54:00 +0900

    init : add Dash in main_gui.py

--- main_gui.py
+++ main_gui.py
@@ -1,27 +1,38 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
 df = px.data.iris()
-fig = px.scatter(df, x="sepal_length", y="sepal_width", color="species")
+col_names = ['s_len', 's_wid', 'p_len', 'p_wid']
 app = Dash(__name__)
 
-# app layout: using by html, dcc module
+
 app.layout = html.Div(children=[
-
-    # make HTML by Dash HTML Components module
-    html.H1(children='headline 1'),
-    html.Div(children='Div'),
-
-    # ploty graph rendering by dash.core.component(dcc)
-    dcc.Graph(id='graph1', figure=fig),
+    html.H1(children='callback practice'),
+    html.Div([
+        'X-var:',
+        dcc.Dropdown(id='xvar_name', value=col_names[0], placeholder='Select X-axis col'),
+        ], style={'width': '30%', 'display': 'inline-block'}),
+    html.Div([
+        'Y-var:',
+        dcc.Dropdown(id='yvar_name', value=col_names[1], placeholder='Select X-axis col'),
+        ], style={'width': '30%', 'display': 'inline-block'}),
+    html.Br(),
+    html.Br(),
+    html.Div([dcc.Graph(id='update_graph')])
 ])
 
+
+@app.callback(
+    Output('update_graph', 'figure'),
+    Input('xvar_name', 'value'),
+    Input('yvar_name', 'value')
+)
+def action_update_graph(xvar, yvar):
+    fig = px.scatter(df, x=xvar, y=yvar, color='species', width=1000, height=700)
+    fig.update_layout(title_text='Scatter plot of ' + xvar + ' vs ' + yvar, title_font_size=30)
+    return fig
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    #app.run_server(debug=False, host='0.0.0.0', port=8888)
-
-
-
-
-
+    app.run_server(debug=False, host='0.0.0.0', port=8888)
