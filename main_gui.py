c7ee87f020f4ce5faa061d8d4e6c6bcb93dc4c09
hanlimy <hanlimy@naver.com>
2023-01-14 15:12:36 +0900

    init : upgrade prediction tab

--- main_gui.py
+++ main_gui.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from dash import Dash, html, dcc, Input, Output
+from dash import Dash, dash_table, html, dcc, Input, Output, State
 import plotly.express as px
 import plotly.graph_objects as go
 
@@ -13,18 +13,25 @@ for mat in list_file_mat:
     box_df_material[mat] = pd.read_csv('smart_collector/output_data/df_{}.csv'.format(mat))
 df_copper = pd.read_csv('smart_collector/output_data/df_copper.csv')
 
+list_info_line = ['LLLA', 'LLLB', 'LLLC']
+list_info_proc = ['PPPA', 'PPPB', 'PPPC']
+list_info_lot = ['BZZ001', 'BZZ002', 'BZZ003']
+list_info_wf = ['0' + str(wf) if wf < 10 else wf for wf in range(1, 25)]
+
 ########################################################################################################################
 
 app = Dash(__name__)
 app.config.suppress_callback_exceptions = True
 
 style_tab = {'height': 20, 'width': 100}
-style_ddn = {'width': '20%', 'display': 'inline-block'}
-style_btn = {'hieght': 20, 'width': 10, 'display': 'inline-block'}
+style_ddn = {'width': 140, 'display': 'inline-block'}
+style_btn = {'height': 20, 'width': 100, 'display': 'inline-block'}
 style_txt_b14 = {'color': 'blue', 'fontSize': 14}
+style_table = {'height': '600px', 'overflowX': 'scroll'}
+style_cell = {'height': '90', 'minWidth': '140px', 'width': '140px', 'maxWidth': '140px', 'whiteSpace': 'normal'},
 
 app.layout = html.Div([
-    dcc.Tabs(id="tabs", value='tab3', children=[
+    dcc.Tabs(id="tabs", value='tab1', children=[
         dcc.Tab(label='prediction', value='tab1', style=style_tab, selected_style=style_tab),
         dcc.Tab(label='analysis', value='tab2', style=style_tab, selected_style=style_tab),
         dcc.Tab(label='ex_price1', value='tab3', style=style_tab, selected_style=style_tab),
@@ -54,44 +61,109 @@ def render_content(tab):
 
 ########################################################################################################################
 
-tab1_box1 = html.Div([
-    html.H1('callback practice : scatter plot for the iris data'),
+
+tab1_box1_dnns = html.Div([
     html.Div([
-        html.H1('X-variable', style=style_txt_b14),
-        dcc.Dropdown(id='tab1_box1_x', options=list_col_iris, value=list_col_iris[0], placeholder='Select X'),
+        html.H1('line', style=style_txt_b14),
+        dcc.Dropdown(id='tab1_box1_ddn1', options=list_info_line, value=list_info_line[0], placeholder='select...'),
     ], style=style_ddn),
     html.Div([
-        html.H1('Y-variable', style=style_txt_b14),
-        dcc.Dropdown(id='tab1_box1_y', options=list_col_iris, value=list_col_iris[1], placeholder='Select Y'),
-    ], style=style_ddn)
+        html.H1('Proc', style=style_txt_b14),
+        dcc.Dropdown(id='tab1_box1_ddn2', options=list_info_wf, value=list_info_wf[0], placeholder='select...'),
+    ], style=style_ddn),
+    html.Div([
+        html.H1('lot_target', style=style_txt_b14),
+        dcc.Dropdown(id='tab1_box1_ddn3', options=list_info_lot, value=list_info_lot[0], placeholder='select...'),
+    ], style=style_ddn),
+    html.Div([
+        html.H1('wf_target', style=style_txt_b14),
+        dcc.Dropdown(id='tab1_box1_ddn4', options=list_info_wf, value=list_info_wf[0], placeholder='select...'),
+    ], style=style_ddn),
+    html.Div([
+        html.H1('lot_base', style=style_txt_b14),
+        dcc.Dropdown(id='tab1_box1_ddn5', options=list_info_lot, value=list_info_lot[0], placeholder='select...'),
+    ], style=style_ddn),
+    html.Div([
+        html.H1('wf_base', style=style_txt_b14),
+        dcc.Dropdown(id='tab1_box1_ddn6', options=list_info_wf, value=list_info_wf[0], placeholder='select...'),
+    ], style=style_ddn),
 ])
 
-tab1_box2 = html.Div([dcc.Graph(id='tab1_box2_graph')])
+tab1_box1_table = html.Div([
+    html.Button('show', id='tab1_box1_btn1', n_clicks=0, style=style_btn),
+    dash_table.DataTable(
+        id='tab1_box1_table',
+        columns=[{'name': idx, 'id': idx, 'deletable': True} for idx in df_copper.columns],
+        data=df_copper.to_dict('records'),
+        style_table=style_table,
+        style_cell=style_cell,
+        # page_current=0,
+        # page_size=1000,
+        # page_action='custom',
+        filter_action='native',
+        # filter_query='',
+        sort_action='native',
+        sort_mode='multi',
+        # sort_by=['Date']
+    )
+])
 
 tab1 = html.Div([
-    tab1_box1,
-    html.Br(),
-    tab1_box2,
+    html.Div([
+        html.H1('prediction'),
+        tab1_box1_dnns,
+        html.Br(),
+        tab1_box1_table,
+    ])
 ])
 
 
 @app.callback(
-    Output('tab1_box2_graph', 'figure'),
-    Input('tab1_box1_x', 'value'),
-    Input('tab1_box1_y', 'value')
+    Output('tab1_box1_table', 'children'),
+    Input('tab1_box1_btn1', 'value'),
+    [State('tab1_box1_ddn{}'.format(num), 'value') for num in range(1, 7)]
 )
-def action_update_graph(xvar, yvar):
-    fig = px.scatter(df_iris, x=xvar, y=yvar, color='species', width=1000, height=700)
-    fig.update_layout(title_text='Scatter plot of ' + xvar + ' vs ' + yvar, title_font_size=30)
-    return fig
+def action_update_graph(btn, dnn1, dnn2, dnn3, dnn4, dnn5, dnn6):
+
+    return tab1_box1_table
 
 
 ########################################################################################################################
 
+tab2_box1_dnns = html.Div([
+    html.Div([
+        html.H1('line', style=style_txt_b14),
+        dcc.Dropdown(id='tab2_box1_ddn1', options=list_info_line, value=list_info_line[0], placeholder='select...'),
+    ], style=style_ddn),
+    html.Div([
+        html.H1('Proc', style=style_txt_b14),
+        dcc.Dropdown(id='tab2_box1_ddn2', options=list_info_wf, value=list_info_wf[0], placeholder='select...'),
+    ], style=style_ddn),
+    html.Div([
+        html.H1('lot_target', style=style_txt_b14),
+        dcc.Dropdown(id='tab2_box1_ddn3', options=list_info_lot, value=list_info_lot[0], placeholder='select...'),
+    ], style=style_ddn),
+    html.Div([
+        html.H1('wf_target', style=style_txt_b14),
+        dcc.Dropdown(id='tab2_box1_ddn4', options=list_info_wf, value=list_info_wf[0], placeholder='select...'),
+    ], style=style_ddn),
+    html.Div([
+        html.H1('lot_base', style=style_txt_b14),
+        dcc.Dropdown(id='tab2_box1_ddn5', options=list_info_lot, value=list_info_lot[0], placeholder='select...'),
+    ], style=style_ddn),
+    html.Div([
+        html.H1('wf_base', style=style_txt_b14),
+        dcc.Dropdown(id='tab2_box1_ddn6', options=list_info_wf, value=list_info_wf[0], placeholder='select...'),
+    ], style=style_ddn),
+])
+
 tab2_box1 = html.Div([
-    dcc.Dropdown(id='tab2_box1_ddn_material', options=list_file_mat, value=list_file_mat[0], placeholder='Select Material',
-                 style=style_ddn),
-    dcc.Graph(id='tab2_box1_graph')
+    html.H1('analysis'),
+    html.Div(id='tab2_box1_dnns', children=[tab2_box1_dnns]),
+    html.Br(),
+    html.Button('show', id='tab1_box1_btn1', n_clicks=0, style=style_btn),
+    html.Br(),
+    html.Div([tab1_box1_table])
 ])
 
 tab2_box2 = html.Div([
@@ -135,7 +207,7 @@ def action_update_graph(btn):
 ########################################################################################################################
 
 tab3_box1 = html.Div([
-    dcc.Dropdown(id='tab3_box1_ddn_material', options=list_file_mat, value=list_file_mat[0], placeholder='Select Material',
+    dcc.Dropdown(id='tab3_box1_ddn_material', options=list_file_mat, value=list_file_mat[0], placeholder='select...',
                  style=style_ddn),
     dcc.Graph(id='tab3_box1_graph')
 ])
@@ -181,7 +253,7 @@ def action_update_graph(btn):
 ########################################################################################################################
 
 tab4_box1 = html.Div([
-    dcc.Dropdown(id='tab4_box1_ddn_material', options=list_file_mat, value=list_file_mat[0], placeholder='Select Material',
+    dcc.Dropdown(id='tab4_box1_ddn_material', options=list_file_mat, value=list_file_mat[0], placeholder='select...',
                  style=style_ddn),
     dcc.Graph(id='tab4_box1_graph')
 ])
